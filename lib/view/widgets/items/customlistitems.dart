579f307e8c59c467468e9c7fb589d04bf9af1faa
lubnagemaa65 <lubnagemaa65@gmail.com>
2022-12-25 13:11:24 +0300

    favourite

--- lib/view/widgets/items/customlistitems.dart
+++ lib/view/widgets/items/customlistitems.dart
@@ -13,87 +13,98 @@ import 'package:work_os/view/linkapi.dart';
 class CustomListItems extends GetView<ItemsControllerImp> {
   final ItemsModel itemsModel;
   final bool active;
-  const CustomListItems( {Key? key, required this.itemsModel,required this.active,}) : super(key: key);
+  const CustomListItems({
+    Key? key,
+    required this.itemsModel,
+    required this.active,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {
-        controller.goToPageProductDetails(itemsModel);
-      },
+        onTap: () {
+          controller.goToPageProductDetails(itemsModel);
+        },
         child: Card(
-      child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              
-              Hero(
-                tag: "${itemsModel.itemsId}",
-                child: CachedNetworkImage(
-                  imageUrl: AppLink.imagestItems + "/" + itemsModel.itemsImage!,
-                  height: 100,
-                  fit: BoxFit.fill,
-                ),
-              ),
-              SizedBox(height: 10) , 
-              Text(translateDataBase(itemsModel.itemsNameAr, itemsModel.itemsName),
-                  style: TextStyle(
-                      color: ColorApp.black,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text("Rating 3.5 ", textAlign: TextAlign.center),
-                  Container(
-                    alignment: Alignment.bottomCenter,
-                    height: 22,
-                    child: Row(
-                      children: [
-                        ...List.generate(
-                            5,
-                            (index) => Icon(
-                                  Icons.star,
-                                  size: 15,
-                                ))
-                      ],
+                  Hero(
+                    tag: "${itemsModel.itemsId}",
+                    child: CachedNetworkImage(
+                      imageUrl:
+                          AppLink.imagestItems + "/" + itemsModel.itemsImage!,
+                      height: 100,
+                      fit: BoxFit.fill,
                     ),
-                  )
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("${itemsModel.itemsPrice} \$",
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                      translateDataBase(
+                          itemsModel.itemsNameAr, itemsModel.itemsName),
                       style: TextStyle(
-                          color: ColorApp.primaryColor,
+                          color: ColorApp.black,
                           fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: "sans")),
-                  GetBuilder<FavouriteController>(builder: (controller)=>IconButton(
-                      onPressed: () {
-                        if(controller.isFavourite[itemsModel.itemsId]=='1'){
-                          controller.setFavourite (itemsModel.itemsId,'0');
-                          controller.removeFavourites(itemsModel.itemsId!);
-                        }else{
-                          controller.setFavourite (itemsModel.itemsId,'1');
-                          controller.addFavourites(itemsModel.itemsId!);
-
-                        }
-                      },
-                      icon: Icon(
-controller.isFavourite[itemsModel.itemsId]=='1'                         
-?  Icons.favorite
-:Icons.favorite_border_outlined,
-
-                        color: ColorApp.primaryColor,
-                      )))
-                ],
-              )
-            ]),
-      ),
-    ));
+                          fontWeight: FontWeight.bold)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Rating 3.5 ", textAlign: TextAlign.center),
+                      Container(
+                        alignment: Alignment.bottomCenter,
+                        height: 22,
+                        child: Row(
+                          children: [
+                            ...List.generate(
+                                5,
+                                (index) => Icon(
+                                      Icons.star,
+                                      size: 15,
+                                    ))
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("${itemsModel.itemsPrice} \$",
+                          style: TextStyle(
+                              color: ColorApp.primaryColor,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              fontFamily: "sans")),
+                      GetBuilder<FavouriteController>(
+                          builder: (controller) => IconButton(
+                              onPressed: () {
+                                if (controller
+                                        .isFavourite[itemsModel.itemsId] ==
+                                    '1') {
+                                  controller.setFavourite(
+                                      itemsModel.itemsId, '0');
+                                  controller
+                                      .removeFavourites(itemsModel.itemsId!);
+                                } else {
+                                  controller.setFavourite(
+                                      itemsModel.itemsId, '1');
+                                  controller.addFavourites(itemsModel.itemsId!);
+                                }
+                              },
+                              icon: Icon(
+                                controller.isFavourite[itemsModel.itemsId] ==
+                                        '1'
+                                    ? Icons.favorite
+                                    : Icons.favorite_border_outlined,
+                                color: ColorApp.primaryColor,
+                              )))
+                    ],
+                  )
+                ]),
+          ),
+        ));
   }
 }
